07a44faf897dbadf08c3c8250e999aa2d84f7e5f
nickmoreton <nick.moreton@torchbox.com>
2024-08-30 11:38:03 +0100

    Update urls import in project template

--- longclaw/project_template/project_name/urls.py
+++ longclaw/project_template/project_name/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    from django.urls.static import static
+    from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
