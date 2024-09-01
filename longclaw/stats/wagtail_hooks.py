3ce1443844ca05958d47cc751edfb16f9c1a3044
nickmoreton <nick.moreton@torchbox.com>
2024-09-01 05:11:51 +0100

    Fixes site summary and stats panel

--- longclaw/stats/wagtail_hooks.py
+++ longclaw/stats/wagtail_hooks.py
@@ -1,71 +1,74 @@
 import datetime
+
+from django.template.loader import render_to_string
+
 from wagtail import hooks
-from wagtail.admin.site_summary import SummaryItem
+from wagtail.admin.ui.components import Component
+
+from longclaw.configuration.models import Configuration
 from longclaw.orders.models import Order
 from longclaw.stats import stats
-from longclaw.configuration.models import Configuration
 from longclaw.utils import ProductVariant, maybe_get_product_model
 
 
-class LongclawSummaryItem(SummaryItem):
-    order = 10
-    template_name = 'stats/summary_item.html'
+class LongclawSiteSummary(Component):
+    order = 100
 
-    def get_context(self):
-        return {
-            'total': 0,
-            'text': '',
-            'url': '',
-            'icon': 'icon-doc-empty-inverse'
-        }
-
-class OutstandingOrders(LongclawSummaryItem):
-    order = 10
-    def get_context(self):
+    def outstanding_orders(self, parent_context):
         orders = Order.objects.filter(status=Order.SUBMITTED)
         return {
-            'total': orders.count(),
-            'text': 'Outstanding Orders',
-            'url': '/admin/orders/order/',
-            'icon': 'icon-warning'
+            "total": orders.count(),
+            "label": "Outstanding Orders",
+            "url": "/admin/orders/order/",
         }
 
-class ProductCount(LongclawSummaryItem):
-    order = 20
-    def get_context(self):
+    def product_count(self, parent_context):
         product_model = maybe_get_product_model()
         if product_model:
             count = product_model.objects.all().count()
         else:
             count = ProductVariant.objects.all().count()
         return {
-            'total': count,
-            'text': 'Product',
-            'url': '',
-            'icon': 'icon-list-ul'
+            "total": count,
+            "label": "Products",
         }
 
-class MonthlySales(LongclawSummaryItem):
-    order = 30
-    def get_context(self):
-        settings = Configuration.for_request(self.request)
+    def sales_count(self, parent_context):
+        settings = Configuration.for_request(parent_context["request"])
         sales = stats.sales_for_time_period(*stats.current_month())
         return {
-            'total': "{}{}".format(settings.currency_html_code,
-                                   sum(order.total for order in sales)),
-            'text': 'In sales this month',
-            'url': '/admin/orders/order/',
-            'icon': 'icon-tick'
+            "total": f"{settings.currency_html_code}{sum(order.total for order in sales)}",
+            "label": "In sales this month",
+            "url": "/admin/orders/order/",
         }
 
-class LongclawStatsPanel(SummaryItem):
+    def render_html(self, parent_context):
+        return render_to_string(
+            "stats/site_summary.html",
+            {
+                "outstanding_orders": self.outstanding_orders(parent_context),
+                "product_count": self.product_count(parent_context),
+                "sales_count": self.sales_count(parent_context),
+            },
+        )
+
+
+@hooks.register("construct_homepage_panels")
+def site_summary_panel(request, panels):
+    panels.append(LongclawSiteSummary())
+
+
+class LongclawStatsPanel(Component):
     order = 110
-    template_name = 'stats/stats_panel.html'
+    template_name = "stats/stats_panel.html"
+
     def get_context(self):
         month_start, month_end = stats.current_month()
         daily_sales = stats.daily_sales(month_start, month_end)
-        labels = [(month_start + datetime.timedelta(days=x)).strftime('%Y-%m-%d')
-                  for x in range(0, datetime.datetime.now().day)]
+        labels = [
+            (month_start + datetime.timedelta(days=x)).strftime("%Y-%m-%d")
+            for x in range(0, datetime.datetime.now().day)
+        ]
         daily_income = [0] * len(labels)
         for k, order_group in daily_sales:
             i = labels.index(k)
@@ -75,24 +78,11 @@ class LongclawStatsPanel(SummaryItem):
         return {
             "daily_income": daily_income,
             "labels": labels,
-            "product_labels": list(popular_products.values_list('title', flat=True)),
-            "sales_volume": list(popular_products.values_list('quantity', flat=True))
+            "product_labels": list(popular_products.values_list("title", flat=True)),
+            "sales_volume": list(popular_products.values_list("quantity", flat=True)),
         }
 
 
-
-
-@hooks.register('construct_homepage_summary_items')
-def add_longclaw_summary_items(request, items):
-
-    # We are going to replace everything with our own items
-    items[:] = []
-    items.extend([
-        OutstandingOrders(request),
-        ProductCount(request),
-        MonthlySales(request)
-    ])
-
-@hooks.register('construct_homepage_panels')
-def add_stats_panel(request, panels):
-    return panels.append(LongclawStatsPanel(request))
+@hooks.register("construct_homepage_panels")
+def statistics_panel(request, panels):
+    panels.append(LongclawStatsPanel())
